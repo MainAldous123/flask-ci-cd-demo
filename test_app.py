239bc0db92cd26e47c5db431b6c00dc695506167
Nando <fernandosurcochoa@gmail.com>
2026-02-24 23:14:10 -0500

    Corrigiendo error de encoding en pruebas

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"¡Hola, CI/CD con DevOps!"
+    assert response.get_data(as_text=True) == "¡Hola, CI/CD con DevOps!"
